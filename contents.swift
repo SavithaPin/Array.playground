713669388afce4a9399436d0e8a918d18c934f52
SavithaPin <savitha@Savithas-MacBook-Air.local>
2016-07-12 04:02:29 -0500

    Array

---
+++ contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var mid = [50, 60, 70, 80]
+var final = [50, 40, 30, 20]
+
+var total = [0,0,0,0]
+
+for i in total
+{
+    total[i] = mid[i] + final[i]
+    print("total = \(total[i])")
+}
